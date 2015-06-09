6964f0b694c874812e776188a25999dfc78c6c83
KostasPapagiannopoulos <KostasPapagiannopoulos@users.noreply.github.com>
2015-06-10 00:36:15 +0300

    test
    tester

--- VirtualEnviromentDjango/hospitalPoject/patient/forms.py
+++ VirtualEnviromentDjango/hospitalPoject/patient/forms.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#hello
 from django import forms
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
